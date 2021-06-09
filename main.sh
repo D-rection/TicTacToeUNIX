f803d387b0d1ecf3bf3dae8a69fcc46fb8c11042
D-rection <artemiy_manakov@mail.ru>
2021-06-09 20:59:01 +0500

    input update

--- main.sh
+++ main.sh
@@ -13,9 +13,30 @@ menu_screen() {
     printf '%s\n' "$DATA"
 }
 
+hello_cycle(){
+    x='';while [[ "$x" != "a" ]]; do read -n1 x; done
+}
 
-opening_screen
-menu_screen
-read -n 1 -s -r -p "Нажмите любую кнопку для продолжения"
+arrow_cycle(){
+    choose=true
+    while $choose
+    do
+        read -r -sn1 t
+        case $t in
+            A) echo up ;;
+            B) echo down ;;
+            C) echo right ;;
+            D) echo left ;;
+            '') echo [CHOISE] ;;
+            'q') echo QUITTING ; choose=false ;;
+        esac
+    done
+}
+
+
+# opening_screen
+# menu_screen
+# read -n 1 -s -r -p "Нажмите любую кнопку для продолжения"
+arrow_cycle
 echo
 exit 0
